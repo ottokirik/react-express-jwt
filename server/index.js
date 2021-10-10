7e8d29873b2f154cdf9b2a1a3631d35f118e443d
Petrov Kirill <ottokirik@gmail.com>
2021-10-10 14:38:31 +0300

    Created mongo models

--- server/index.js
+++ server/index.js
@@ -4,11 +4,19 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const mongoose = require('mongoose');
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL);
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.log(error);

91f1b23e1630e46f4343b0264017ce41557b439d
Petrov Kirill <ottokirik@gmail.com>
2021-10-11 01:22:48 +0300

    Validation included

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,17 @@
 const userService = require('../srvice/user-service');
+const { validationResult } = require('express-validator');
+
+const ApiError = require('../exceptions/api-error');
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequestError('Ошибка валидации', errors.array()));
+      }
+
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
 
--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 
+const { body } = require('express-validator');
+
 const userController = require('../controllers/user-controller');
 
 const router = new Router();
 
-router.post('/registration', userController.registration);
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 5, max: 32 }),
+  userController.registration
+);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 

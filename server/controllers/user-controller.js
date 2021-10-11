a349e251944381b54c51e6511b749cf005310b32
Petrov Kirill <ottokirik@gmail.com>
2021-10-12 00:16:25 +0300

    Logout functionallity

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -15,7 +15,7 @@ class UserController {
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
 
-      res.cookie('refreshToke', userData.refreshToken), { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true };
+      res.cookie('refreshToken', userData.refreshToken), { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true };
 
       return res.json(userData);
     } catch (error) {
@@ -29,7 +29,7 @@ class UserController {
 
       const userData = await userService.login(email, password);
 
-      res.cookie('refreshToke', userData.refreshToken), { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true };
+      res.cookie('refreshToken', userData.refreshToken), { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true };
 
       return res.json(userData);
     } catch (error) {
@@ -39,6 +39,15 @@ class UserController {
 
   async logout(req, res, next) {
     try {
+      const { refreshToken } = req.cookies;
+
+      console.log(req.cookies);
+
+      const token = await userService.logout(refreshToken);
+
+      res.clearCookie('refreshToken');
+
+      return res.json(token);
     } catch (error) {
       next(error);
     }
--- server/srvice/token-service.js
+++ server/srvice/token-service.js
@@ -23,6 +23,12 @@ class TokenService {
 
     return token;
   }
+
+  async removeToken(refreshToken) {
+    const tokenData = await tokenModel.deleteOne({ refreshToken });
+
+    return tokenData;
+  }
 }
 
 module.exports = new TokenService();
